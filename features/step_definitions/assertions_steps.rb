eb16716e3629ff41818bfd5cb90bc282f811f5be
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-17 11:44:51 +0100

    adds new article form and adds cucumber skeletopn for additional features

---
+++ features/step_definitions/assertions_steps.rb
@@ -0,0 +1,7 @@
+Then("I should see {string}") do |content|
+	expect(page).to have_content content
+end
+
+Then("I should be on {string} page") do |string|
+expect(page).to have_content string
+end
---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,25 @@
+When("I visit the site") do
+	visit root_path
+end
+
+Given("the following articles exists") do |table|
+	table.hashes.each do |article|
+		Article.create!(article)
+	end
+end
+
+Given("I visit the {string} page") do |string|
+	visit root_path
+end
+
+When("I click {string} button") do |button|
+	click_on button
+end
+
+When("I fill in {string} with {string}") do |field, input|
+	fill_in field, with: input
+end
+
+When("I click {string}") do |string|
+	click_on string
+end
